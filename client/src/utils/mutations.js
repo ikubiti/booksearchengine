0da9828bf5b5c2c5d6b010acb5a80037f14ee886
ikubiti <ikubiti@icloud.com>
2023-04-25 13:37:28 -0400

    defined mutations for the front-end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			_id
+			username
+			email
+		}
+	}
+`;
+
+export const ADD_BOOK = gql`
+	mutation saveBook($authors: [String], $description: String!, $bookId: String!, $image: String, $link: String, $title: String!) {
+		saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+			_id
+			username
+			email
+			savedBooks {
+				_id
+				authors
+				bookId
+				image
+				link
+				title
+			}
+		}
+	}
+`;
+
+export const DELETE_BOOK = gql`
+	mutation deleteBook($bookId: String) {
+		deleteBook(bookId: $bookId) {
+			_id
+			username
+			email
+			savedBooks {
+				_id
+				authors
+				bookId
+				image
+				link
+				title
+			}
+		}
+	}
+`;
+
+export const LOGIN_USER = gql`
+	mutation login($email: String, $username: String, $password: String!) {
+		login(email: $email, username: $username, password: $password) {
+			token
+			user {
+			_id
+			username
+			}
+		}
+	}
+`;
